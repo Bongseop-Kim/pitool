8b39ea41ce076b6b63d0bd29652e65773e57337a
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2022-03-09 17:20:42 +0900

    trainer-routine-exersie
    루틴에 운동 설정하는 기능 추가했덩

--- js/js.js
+++ js/js.js
@@ -30,3 +30,81 @@ $('.routine-name').on('click', function(){
     $('.list-group-routine-name').toggleClass('show');
 })
 // 등록된 루틴 확인
+
+
+$('.exercise-plus-list').on('click', function(){
+    $('.exercise-list').toggleClass('show');
+})
+// 운동 리스트 확인 트레이너
+
+
+
+
+
+  function method()  {
+    settingCount = document.getElementById('setting-value').value;
+    document.getElementById('kgcountlist').innerHTML = '';
+    for (var i = 0; i < settingCount; i++){
+        템플릿 = `<div class="order${i}">${i+1}set  <span class='input-kg show'><input class="input-box"/>kg</span>  
+        <span class='input-count show'><input class="input-box"/>개</span>  
+        <span class='input-time show'><input class="input-box"/><span class="hr-mi-se">초</span></span> <br><br>`;
+        $('#kgcountlist').append(템플릿);
+    } 
+  };
+
+
+  $('.btn-kg-count').on('click', function(){
+      $('.input-kg').removeClass('none');
+    $('.input-kg').addClass('show');
+    $('.input-count').removeClass('none');
+    $('.input-count').addClass('show');
+    $('.input-time').removeClass('show');
+    $('.input-time').addClass('none');
+    $('.time-set').html('')
+});
+  
+$('.btn-count').on('click', function(){
+    $('.input-kg').removeClass('show');
+    $('.input-kg').addClass('none');
+    $('.input-count').removeClass('none');
+    $('.input-count').addClass('show');
+    $('.input-time').removeClass('show');
+    $('.input-time').addClass('none');
+    $('.time-set').html('')
+});
+
+
+$('.btn-time').on('click', function(){
+    $('.input-kg').removeClass('show');
+    $('.input-kg').addClass('none');
+    $('.input-count').removeClass('show');
+    $('.input-count').addClass('none');
+    $('.input-time').removeClass('none');
+    $('.input-time').addClass('show');
+    $('.time-set').html('')
+    const time = `<br><button class="btn btn-info btn-hours" type="button" >시간</button>
+      <button class="btn btn-info btn-minutes">분</button>
+      <button class="btn btn-info btn-seconds">초</button>`
+      $('.time-set').append(time)
+      $('.btn-hours').on('click',function(){
+      $('.hr-mi-se').html('시간');
+      })
+      $('.btn-minutes').on('click',function(){
+      $('.hr-mi-se').html('분');
+    })
+      $('.btn-seconds').on('click',function(){
+      $('.hr-mi-se').html('초');
+    })
+})
+
+$('.all-set').on('click', function(){
+    for ( var i = 0; i < settingCount-1; i++){
+    $('.order'+i).addClass('none')
+    }
+})
+
+$('.different-set').on('click', function(){
+    for ( var i = 0; i < settingCount-1; i++){
+    $('.order'+i).removeClass('none')
+    }
+})
